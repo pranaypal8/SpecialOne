07cad864b166662b0c907ae77be085f8ea4a225e
pranaypal8 <132996855+pranaypal8@users.noreply.github.com>
2025-06-14 22:44:01 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        anurati: ['Anurati', 'sans-serif'],
+        Playfair : ['Playfair' , 'sans-serif']
+      },
+      colors: {
+        lightGray: '#F2F0F1',
+      },
+    },
+  },
+  plugins: [],
+}
